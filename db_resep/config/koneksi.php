628c8d037314f3e8491e6909b13f26c17870b998
basri12 <33261615+basri12@users.noreply.github.com>
2017-10-31 23:23:08 +0700

    Update koneksi.php
    coment

--- db_resep/config/koneksi.php
+++ db_resep/config/koneksi.php
@@ -5,7 +5,7 @@ $server = "localhost";
 $username = "root";
 $password = "";
 $database = "db_resep";
-
+//cek koneksi
 // Koneksi dan memilih database di server
 $con =mysqli_connect($server,$username,$password,$database) or die("Koneksi gagal");
 //mysqli_select_db($database) or die("Database tidak bisa dibuka");
