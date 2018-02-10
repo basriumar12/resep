533b0446c100b086986dfec2eb05de064b66e310
Bas Bas <basriumar12@gmail.com>
2018-02-11 06:12:46 +0700

    Create insert.php

---
+++ db_resep/insert.php
@@ -0,0 +1,22 @@
+<?php 
+ include './config/koneksi.php';
+
+ if($_SERVER['REQUEST_METHOD'] == 'POST')
+ {
+ 	//parameter post
+ 	$nama_resep = $_POST['nama_resep'];
+ 	$detail = $_POST['detail'];
+ 	$gambar = $_POST['gambar'];
+
+ 	$query = "INSERT INTO tb_resep (nama_resep, detail,gambar) VALUES ('$nama_resep','$detail','$gambar')";
+
+ 	$exeQuery = mysqli_query($con, $query); 
+
+ 	echo ($exeQuery) ? json_encode(array('kode' =>1, 'pesan' => 'berhasil menambahkan data')) :  json_encode(array('kode' =>2, 'pesan' => 'data gagal ditambahkan'));
+ }
+ else
+ {
+ 	 echo json_encode(array('kode' =>101, 'pesan' => 'request tidak valid'));
+ }
+
+ ?>
